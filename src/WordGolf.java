759535512b0e9264c3611a385aa8d0a805d1b2b4
Sterling Sutton <sterlingsutton2006@gmail.com>
2023-01-23 19:13:58 -0800

    Initial commit

---
+++ src/WordGolf.java
@@ -0,0 +1,36 @@
+public class WordGolf {
+    // Method that takes a String in and returns its point value
+    public static int parseWord(String word) {
+        int points = 1;
+        String vowels = "aeiouy";
+        String consonants = "qwrtpsdfghjklzxcvbnm";
+        for (int i = 0; i < word.length(); i++) {
+            String currentChar = word.substring(i, i + 1).toLowerCase();
+            if (vowels.contains(currentChar)) {
+                points *= 2;
+            }
+            else if (consonants.contains(currentChar)) {
+                points += 1;
+            }
+        }
+        return points;
+    }
+
+    // Method that splits up a sentence into words and calls the parseWord method on each respective word
+    public static int parseSentence(String sentence) {
+        int points = 0;
+        int startIndex = 0;
+        int endIndex = 0;
+        for(int i = 0; i < sentence.length(); i++) {
+            String currentChar = sentence.substring(i, i + 1);
+            // If the currentChar is a space or the entire sentence has been iterated through then parse the last word
+            if (currentChar.contains(" ") || i == sentence.length() - 1) {
+                endIndex = i;
+                points += parseWord(sentence.substring(startIndex, endIndex + 1));
+                // Set startIndex to endIndex so that the next word starts where the last word ended
+                startIndex = endIndex;
+            }
+        }
+        return points;
+    }
+}
---
+++ src/WordGolfRunner.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class WordGolfRunner {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        System.out.println(WordGolf.parseSentence("Java Google"));
+    }
+}
