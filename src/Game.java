d3031f8174a7a8793924308c6574006a9a4ae410
Sterling Sutton <sterlingsutton2006@gmail.com>
2023-01-24 21:16:11 -0800

    Add some stuff

--- src/Game.java
+++ src/Game.java
@@ -5,6 +5,15 @@ public class Game {
     public Game() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Word Golf!");
+        System.out.println("Rules :");
+        System.out.println(" 1.) Each player enters a sentence per turn.");
+        System.out.println(" 2.) From left to right, if there's a consonant then add 1,");
+        System.out.println("     if there's a vowel then multiply by 2.");
+        System.out.println(" 3.) Add the points to the total for the player.");
+        System.out.println(" 4.) If the player reaches exactly 100 points then they win.");
+        System.out.println("     If they overshoot then the points will subtract instead of add.");
+        System.out.println(" NOTE: If at any time during the game you would like to quit");
+        System.out.println("     then simply enter QUIT in all caps and you will be removed from the game.");
         // Ask how many players will be playing
         System.out.print("How many players? (1-10) : ");
         int numPlayers = 0;
@@ -23,5 +32,7 @@ public class Game {
         for (Player player : players) {
             player = new Player();
         }
+
+
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 public class Player {
+    private boolean participating = true;
+    private int strokeNum = 0;
     private static int playerCount = 0;
     private int numSentences;
     private int totalPoints;
     private String name;
+
+    public boolean isParticipating() {
+        return participating;
+    }
 
     public Player() {
         Scanner scanner = new Scanner(System.in);
@@ -13,5 +19,34 @@ public class Player {
         // Ask for the user's name
         System.out.print("Enter a name for player #" + playerCount + " : ");
         name = scanner.nextLine();
+    }
+
+    public void stroke() {
+        Scanner scanner = new Scanner(System.in);
+        String sentence = "";
+        // Increment the strokeNum
+        strokeNum++;
+        System.out.print("Enter your sentence for this round : ");
+        sentence = scanner.nextLine();
+        if (sentence.equals("QUIT")) {
+
+        }
+    }
+
+    // Terrorize the user if they desire to quit ;)
+    private void lameQuitterMessage() {
+        participating = false;
+        String[] messageVariants = {
+                "I'm disappointed in you!",
+                "Leaving so soon?",
+                "What a party pooper!",
+                "How lame of you!",
+                "Gosh that killed the mood!",
+                "We were having fun until you ruined it!",
+                "Word golf isn't your jam?"
+        };
+
+        // Print a random message from the messageVariants list
+        System.out.println(messageVariants[(int)(Math.random() * messageVariants.length)]);
     }
 }
